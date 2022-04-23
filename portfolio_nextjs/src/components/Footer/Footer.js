87341164e1410376835414fa138dfa769b6c0ede
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-04-23 17:29:29 +0530

    completed

--- portfolio_nextjs/src/components/Footer/Footer.js
+++ portfolio_nextjs/src/components/Footer/Footer.js
@@ -7,7 +7,27 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 const Footer = () => {
   return (
     <FooterWrapper>
-      
+      <LinkList>
+      <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:Khandelwalmanan05@gmail.com">
+            Khandelwalmanan05@gmail.com
+          </LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Innovating one project at a time</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com/mnnkhndlwl">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://www.linkedin.com/in/manan-khandelwal-327836167/">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
     </FooterWrapper>
   );
 };
