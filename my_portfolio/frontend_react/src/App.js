9a181b14e6f7fd5d3586a2f5e686ca309e0deaed
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-04-24 17:09:21 +0530

    commit

--- my_portfolio/frontend_react/src/App.js
+++ my_portfolio/frontend_react/src/App.js
@@ -4,7 +4,12 @@ import {Work,Footer,Header,Skills,Testimonial,About} from './container/index'
 const App = () => {
   return (
     <div className="app">
-      
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
     </div>
   );
 }
