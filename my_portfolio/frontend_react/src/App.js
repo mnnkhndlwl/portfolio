50c5407692f421b1bde9b9737519a635c47bee44
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-04-24 22:10:32 +0530

    navbar completed

--- my_portfolio/frontend_react/src/App.js
+++ my_portfolio/frontend_react/src/App.js
@@ -1,9 +1,11 @@
 import {Work,Footer,Header,Skills,Testimonial,About} from './container/index'
-
+import {Navbar} from './components/index';
+import "./App.scss";
 
 const App = () => {
   return (
     <div className="app">
+    <Navbar />
       <Header />
       <About />
       <Work />
