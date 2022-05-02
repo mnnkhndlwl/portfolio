78fc4454a668e29ff3f8279df8b24bed22b010f6
mnnkhndlwl <khandelwalmanan05@gmail.com>
2022-05-02 10:55:02 +0530

    commit

--- my_portfolio/frontend_react/src/App.js
+++ my_portfolio/frontend_react/src/App.js
@@ -1,57 +1,18 @@
 import {Projects,Footer,Header,Skills,About} from './container/index'
 import {Navbar} from './components/index';
 import "./App.scss";
-import { useState,useEffect } from 'react';
-import { motion } from 'framer-motion';
 
 const App = () => {
-
-  const [mousePosition, setMousePosition] = useState({
-    x: 50,
-    y: 50
-  });
-
-  const [cursorvariants,setcursorvariants ] = useState("default");
-
-  useEffect(() => {
-    const mouseMove = e => {
-      setMousePosition({
-        x: e.clientX,
-        y: e.clientY
-      })
-    }
-
-    window.addEventListener("mousemove", mouseMove);
-
-    return () => {
-      window.removeEventListener("mousemove", mouseMove);
-    }
-  }, []);
-
-  const variants = {
-    default: {
-      x: mousePosition.x - 16,
-      y: mousePosition.y - 16,
-    },
-  }
-  
   return (
     <>
     <div className="app">
-    <motion.div
-        className='cursor'
-        variants={variants}
-        animate={cursorvariants}
-      />
     <Navbar />
       <Header />
-      <div className='kuch'>
       <About />
       <Projects />
       <Skills />
       <Footer />
       </div>
-    </div>
     </>
   );
 }
